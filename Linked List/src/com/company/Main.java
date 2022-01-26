7ec766f39bfff9392f1265e7f3178df80c721a9d
Seth Reeise <southsc22@gmail.com>
2022-01-26 01:29:18 -0500

    Implementation

--- Linked List/src/com/company/Main.java
+++ Linked List/src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+/*
+ * Singly Linked List
+ *
+ * Created by: Seth Reeise
+ * January 26th 2022
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyLinkedList linkedList = new MyLinkedList();
+
+        linkedList.addToStart(12);
+        linkedList.addToStart(18);
+        linkedList.addToStart(99);
+
+        System.out.println(linkedList.toStringBuilder());
+
+        linkedList.addToEnd(350);
+        linkedList.addToEnd(55);
+
+        System.out.println(linkedList.toStringBuilder());
     }
 }
---
+++ Linked List/src/com/company/MyLinkedList.java
@@ -0,0 +1,64 @@
+package com.company;
+
+/*
+* Singly Linked List
+*
+* Created by: Seth Reeise
+* January 26th 2022
+ */
+
+// This is my manual implementation of a singly linked list in Java
+
+public class MyLinkedList {
+
+    // Used as a struct
+    public class Node {
+        int value;
+        Node next;
+    }
+
+    // Create head node
+    Node head;
+
+    // Add new node to the start of the linked list
+    void addToStart(int newNodeValue) {
+        // Create new node
+        Node newNode = new Node();
+        newNode.value = newNodeValue;
+
+        // Assign tmp node's next "pointer" to the current head node therefore placing it in front of the list
+        newNode.next = head;
+
+        // Assign as head node
+        head = newNode;
+    }
+
+    // Add new node to the end of the linked list
+    void addToEnd(int newNodeValue) {
+        // Create new node
+        Node newNode = new Node();
+        newNode.value = newNodeValue;
+
+        // Assign tmp node to head to traverse through linked list
+        Node tmp = head;
+
+        // Traverse through linked list until the end is reached
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+
+        // Assign new node to the end of the list
+        tmp.next = newNode;
+    }
+
+    public StringBuilder toStringBuilder() {
+        Node tmp = head;
+        StringBuilder result = new StringBuilder();
+
+        while (tmp != null) {
+            result.append(Integer.toString(tmp.value)).append(" - ");
+            tmp = tmp.next;
+        }
+        return result;
+    }
+}
